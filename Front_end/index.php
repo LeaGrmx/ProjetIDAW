62cc885a026a7e93d867f395379a3a7b88b59ba4
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-03-25 11:00:58 +0100

    architecture site

---
+++ Front_end/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Mon profil iMangerMieux </title>
+    </head>
+    <body>
+    Bonjour ! Nous sommes le :
+        <?php
+        // Affichage de la date
+        echo date("d/m/Y");?>
+
+    </body>
+
+</html>
