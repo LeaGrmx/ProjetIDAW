48f07b7a8073d0faa91b989bbad35371a1a83677
pirmartin <pierre.martin@etu.imt-nord-europe.fr>
2022-03-25 12:16:43 +0100

    fin

--- Front_end/journal.php
+++ Front_end/journal.php
@@ -1,1 +1,17 @@
-<p> ceci est un journal</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Journal</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet_journal" href="cv.css">
+    </head>
+<body>
+    <h1>REPAS</h1>
+    <form method="post" id="addNewRepas" action="" onsubmit="onFormSubmit();">
+        <div class="form-group row">
+            <label for="input>
+
+
+
+
+        
