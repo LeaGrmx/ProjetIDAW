a3f00af2cbac2f39fcc8bcd9d829aec1a0e8e2d3
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-04-02 16:21:07 +0200

    .

--- Front_end/newaliment.php
+++ Front_end/newaliment.php
@@ -200,7 +200,7 @@
                         <div class="col-lg-6 mb-4">
                             <div class="card shadow mb-4">
                                 <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-danger">Créer un nouvel aliment dans la base de données.</h6>
+                                    <h6 class="m-0 font-weight-bold text-danger">Créer un nouvel aliment dans la base de données</h6>
                                 </div>
                                 <div class="card-body">
                                     <p>Vous pouvez ici ajouter les aliments que vous souhaitez dans notre base de données.</p>
