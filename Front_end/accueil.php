4a680dc0499a68c846e1ac3e3d7c1d75ed2c6e0f
LeaGrmx <lea.grumiaux@etu.imt-nord-europe.fr>
2022-04-03 18:25:26 +0200

    accueil

--- Front_end/accueil.php
+++ Front_end/accueil.php
@@ -258,10 +258,10 @@
                                 </div>
                                 <div class="card-body">
                                     <h4 class="small font-weight-bold">Ajout utilisateur <span
-                                            class="float-right">20%</span></h4>
+                                            class="float-right">100%</span></h4>
                                     <div class="progress mb-4">
-                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 20%"
-                                            aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 100%"
+                                            aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                     </div>
                                     <h4 class="small font-weight-bold">Page d'accueil <span
                                             class="float-right">40%</span></h4>
